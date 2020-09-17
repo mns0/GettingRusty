a03d110fd2c3efc8018379052bd6bec20c19dcba
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-09-17 11:14:11 -0700

    base code

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+// src/main.rs
+#[macro_use]
+extern crate diesel;
+
+use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+//for jwt middleware
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
+
+mod auth;
+mod errors;
+mod handlers;
+mod models;
+mod schema;
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+//jwt validator
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.get_ref().clone())
+        .unwrap_or_else(Default::default);
+    match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res == true {
+                Ok(req)
+            } else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into()),
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    //create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Faild to create pool.");
+
+    //start http server
+    HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(validator);
+        App::new()
+            .wrap(auth)
+            .data(pool.clone())
+            .route("/users", web::get().to(handlers::get_users))
+            .route("/users/{id}", web::get().to(handlers::get_user_by_id))
+            .route("/users", web::post().to(handlers::add_user))
+            .route("/users/{id}", web::delete().to(handlers::delete_user))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
